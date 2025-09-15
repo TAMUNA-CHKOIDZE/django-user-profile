6f31b3a132425d930d7d66fdcfb0039e59734cb6
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-09-15 19:10:40 +0400

    Update README.md

--- userprofile_project/profiles/urls.py
+++ userprofile_project/profiles/urls.py
@@ -4,6 +4,6 @@ from profiles.views import profile_list, profile_detail
 app_name = 'profiles'
 
 urlpatterns = [
-    path('users/', profile_list, name='user_list'),  # ყველა მომხმარებლის სია
-    path('users/<int:pk>/', profile_detail, name='profile_detail'),  # კონკრეტული პროფილი
+    path('', profile_list, name='user_list'),  # ყველა მომხმარებლის სია
+    path('<int:pk>/', profile_detail, name='profile_detail'),  # კონკრეტული პროფილი
 ]
